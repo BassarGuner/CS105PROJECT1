2995b6edf4b61a36c4d18792678ed195a455662c
mardaguler <arda.guler@ozu.edu.tr>
2024-12-10 10:29:05 +0300

    ekliom gene gene

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ package src;
 public class Main {
      public static void main(String[] args) {
           System.out.println("Selamun Aleykum");
+          int a = 29;
+          System.out.println("Zort: " + a);
           int x = 5;
      }
 }
