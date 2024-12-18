1e2fbf3a2b29a0adc1405e1845a8fcc8b0af70ca
melotako <altundasmeltem@gmail.com>
2024-12-18 22:40:58 +0300

    Attributes, Constructor, ve 1 method ekledim. method-2'de yarım kaldım.

--- Meltem.java
+++ Meltem.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+
 public class Meltem {
-    
-}
+    // Attributes
+    private String id;
+    private String name;
+    private Instructor instructor; // Instructor bir class aslında o classı sonra yapçam
+    private ArrayList<Student> students;
+
+    // Constructor
+    public Meltem (String courseID, String courseName) {
+        id = courseID; // this kullansam daha mı anlaşılır olurdu?
+        name = courseName;
+        students = new ArrayList<>(); // başta öğrenci boş liste
+        instructor = null; // başta hoca yok
+    }
+
+    // Method 1 - Add a student to the course
+    public void addStudent(Student student) {
+        if (student != null) {
+            students.add(student);
+            System.out.println(student.getName() + " added to " + name ); } // buradaki getter Student sınıfının içinden geliyor aslında, o yüzden Student class'ta tanımlanmalı getter!
+        else {
+            System.out.println("Error: student is null"); }
+    }
+
+    // Method 2 - Remove a student from the course
+    public void removeStudent(Student student) {
+        if ()
+    }
+
+    }
+
