c094c2091b18b1ca9e419c9d059aec026e77a91b
melotako <altundasmeltem@gmail.com>
2024-12-20 02:10:33 +0300

    Course class TAMAMLANDI !

--- Meltem.java
+++ Meltem.java
@@ -21,13 +21,68 @@ public class Meltem {
             students.add(student);
             System.out.println(student.getName() + " added to " + name ); } // buradaki getter Student sınıfının içinden geliyor aslında, o yüzden Student class'ta tanımlanmalı getter!
         else {
-            System.out.println("Error: student is null"); }
+            System.out.println("Error: Student is null"); }
     }
 
     // Method 2 - Remove a student from the course
     public void removeStudent(Student student) {
-        if () // if elseler
+        if (students.contains(student)) {
+            students.remove(student);
+            System.out.println(student.getName() + " removed from " + name);
+        }
+        else {
+            System.out.println("Error: Student not found in the " + name);
+        }
     }
 
+    // Getters and Setters:
+
+        // Getter1 - getID()
+    public String getId() {
+        return id;
     }
+
+        // Getter2 - getName()
+    public String getName() {
+        return name;
+    }
+
+        // Getter3 - getInstructor()
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+        // Getter4 - getStudents()
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+
+        // Setter1 - setInstructor()
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+        System.out.println("Instructor " + instructor.getName() + " assigned to course " + name);
+    }
+
+    // Reports (Prints):
+
+        // Prints the names of all enrolled students
+    public void listStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students enrolled in " + name); }
+        else {
+            System.out.println("Students enrollen in " + name + ":");
+            for (Student s : students) {
+                System.out.println("Name: " + s.getName());
+                }
+            }
+        }
+
+        // Prints course details
+    @Override
+    public String toString() {
+        return "Course ID = " + id + " Course Name = " + name + " Instructor = " + instructor;
+        + (instructor != null ? instructor.getName() : "None"); // koşul ? doğruysa_dönen_değer : yanlışsa_dönen_değer
+    }
+}
 
